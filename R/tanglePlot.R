f434bbfa686f03137490a35d7e24114d24e8f130
Joseph Crispell <crispelljoseph@gmail.com>
2019-11-28 15:49:33 +0000

    Adding in documentation

--- R/tanglePlot.R
+++ R/tanglePlot.R
@@ -24,32 +24,33 @@
 #setwd("..")
 #install("phyloHelpeR")
 
-
-
-#### Preparation ####
-
-# Load libraries
-library(ape)
-library(grid)
-
-# Set the seed
-set.seed(76263)
-
-#### Create a random phylogeny and rotate a node ####
-
-# Create a random phylogeny
-tree <- rtree(n=50, rooted=TRUE)
-
-# Rotate one of the nodes in the phylogeny - with tip labels it
-rotated <- rotate(tree, node=c("t39", "t46"))
-
-#### Compare the random and rotated phylogeny ####
-
-tanglePlot(tree, rotated, connectingLine.col="red",
-           connectingLine.lty=2, show.tip.label=TRUE, offsetProp=0.02)
-
-#### FUNCTIONS ####
-
+#' Plot the different nucleotides present in an alignment of nucleotide sequences
+#'
+#' Function uses coloured polygons to illustrate which nucleotides are present in each sequence at each position
+#' @param tree1 an object of class "phylo" to compared to \code{tree2}
+#' @param tree2 an object of class "phylo" to compared to \code{tree1}
+#' @param onlyIfDifferent a boolean variable, if \code{TRUE} only plots lines between tips that are in different locations on the phylogenies, otherwise lines between all tips are plotted. Defaults to \code{TRUE}
+#' @param connectingLine.col colour of the lines connecting tips. Defaults to black
+#' @param connectingLine.lwd width of the lines connecting tips. Defaults to 1
+#' @param connectingLine.lty type of the lines connecting tips. Defaults to 1 (solid)
+#' @param offsetProp offset value to displace starts of connecting lines from tips, set as the proportion of plotting window. Defaults to \code{NULL} (off)
+#' @param leftMargins margin parameters for the left tree plot. Defaults to \code{c(0,0,0,1)} to leave small space on right
+#' @param rightMargins margin parameters for the right tree plot. Defaults to \code{c(0,1,0,0)} to leave small space on left
+#' @keywords pylogeny tree tangle plot compare
+#' @export
+#' @examples 
+#' # Set the seed
+#' set.seed(76263)
+#' 
+#' # Create a random phylogeny
+#' tree <- ape::rtree(n=50, rooted=TRUE)
+#' 
+#' # Rotate one of the nodes in the phylogeny - with tip labels it
+#' rotated <- ape::rotate(tree, node=c("t39", "t46"))
+#' 
+#' # Compare the random and rotated phylogeny ####
+#' tanglePlot(tree, rotated, connectingLine.col="red",
+#'            connectingLine.lty=2, show.tip.label=TRUE, offsetProp=0.02)
 tanglePlot <- function(tree1, tree2, onlyIfDifferent=TRUE,
                        connectingLine.col="black",
                        connectingLine.lwd=1,
@@ -69,14 +70,14 @@ tanglePlot <- function(tree1, tree2, onlyIfDifferent=TRUE,
   par(mar=leftMargins)
 
   # Plot phylogeny 1
-  plot.phylo(tree1, direction="rightwards", ...)
+  ape::plot.phylo(tree1, direction="rightwards", ...)
   tree1TipCoordinates <- getTipCoordinates(tree1$tip.label)
 
   # Set the plotting margin - leave space on right
   par(mar=rightMargins)
 
   # Plot phylogeny 2
-  plot.phylo(tree2, direction="leftwards", ...)
+  ape::plot.phylo(tree2, direction="leftwards", ...)
   tree2TipCoordinates <- getTipCoordinates(tree2$tip.label)
 
   # Plot lines between the phylogenies
@@ -94,13 +95,24 @@ tanglePlot <- function(tree1, tree2, onlyIfDifferent=TRUE,
   par(mfrow=currentMfrow)
 }
 
+#' Plot lines between coordinates of tips on phylogenies
+#'
+#' Function used by \code{tanglePlot()}
+#' @param tipLocationsA a list of coordinates of tips on a phylogenetic tree plot
+#' @param tipLocationsB a list of coordinates of tips on a phylogenetic tree plot
+#' @param col colour of the lines connecting tips. Defaults to black
+#' @param lwd width of the lines connecting tips. Defaults to 1
+#' @param lty type of the lines connecting tips. Defaults to 1 (solid)
+#' @param onlyIfDifferent a boolean variable, if \code{TRUE} only plots lines between tips that are in different locations on the phylogenies, otherwise lines between all tips are plotted. Defaults to \code{TRUE}
+#' @param offsetProp offset value to displace starts of connecting lines from tips, set as the proportion of plotting window. Defaults to \code{NULL} (off)
+#' @keywords internal
 plotLinesBetweenTips <- function(tipLocationsA, tipLocationsB,
                                  col="black", lwd=1, lty=1,
                                  onlyIfDifferent=TRUE, offsetProp=NULL){
 
   # Open up the port to allow lines to be added
-  pushViewport(viewport())
-  popViewport()
+  grid::pushViewport(viewport())
+  grid::popViewport()
 
   # Calculate the offset for the X positions
   xOffset <- 0
@@ -134,21 +146,36 @@ plotLinesBetweenTips <- function(tipLocationsA, tipLocationsB,
   }
 }
 
+#' Plot lines between sets of coordinates in the plotting window
+#'
+#' Function used by \code{tanglePlot()}
+#' @param coordsA a list of coordinates on an existing plot
+#' @param coordsB a list of coordinates on an existing plot
+#' @param col colour of the lines connecting tips. Defaults to black
+#' @param lwd width of the lines connecting tips. Defaults to 1
+#' @param lty type of the lines connecting tips. Defaults to 1 (solid)
+#' @param xOffset The distance on X axis to displace the start and ends of connecting lines. Defaults to 0
+#' @keywords internal
 plotLineBetweenTips <- function(coordsA, coordsB,
                                 col="black", lwd=1, lty=1, xOffset=0){
 
   # Open up the port to plot the current line
-  pushViewport(viewport())
+  grid::pushViewport(viewport())
 
   # Plot a connecting line between tip on left and same tip on right
-  grid.lines(x = c(coordsA[1]+xOffset, coordsB[1]-xOffset),
-             y = c(coordsA[2], coordsB[2]),
-             gp = gpar(col=col, lty=lty, lwd=lwd))
+  grid::grid.lines(x = c(coordsA[1]+xOffset, coordsB[1]-xOffset),
+                   y = c(coordsA[2], coordsB[2]),
+                   gp = gpar(col=col, lty=lty, lwd=lwd))
 
   # Close the port
-  popViewport()
+  grid::popViewport()
 }
 
+#' Get the relative and absolute coordinates of tips on a plotted phylogeny
+#'
+#' Function used by \code{tanglePlot()}
+#' @param tipLabels a vector of the tip labels of a plotted phylogeny, which can be retrieved from a "phylo" object using: \code{tree$tip.label}
+#' @keywords internal
 getTipCoordinates <- function(tipLabels){
 
   # Get the tip coordinates in the last phylogeny plot
